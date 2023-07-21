f7a0f803709c09c1df08691f6e6fd38e02fa3ce4
Don Mahurin <@>
2023-07-20 21:42:40 -0700

    Copy and combine code from functions setup_model_parallel and load from example.py into new 'build' functon in llama/generate.py.
    This better aligns with Llama 2.

--- llama/generation.py
+++ llama/generation.py
@@ -7,9 +7,55 @@ import torch
 
 from llama.model import Transformer
 from llama.tokenizer import Tokenizer
+import os
 
 
 class LLaMA:
+    @staticmethod
+    def build(
+        ckpt_dir: str,
+        tokenizer_path: str,
+        max_seq_len: int,
+        max_batch_size: int,
+    ) -> "LLaMA":
+        local_rank = int(os.environ.get("LOCAL_RANK", -1))
+        world_size = int(os.environ.get("WORLD_SIZE", -1))
+
+        torch.distributed.init_process_group("nccl")
+        initialize_model_parallel(world_size)
+        torch.cuda.set_device(local_rank)
+
+        # seed must be the same in all processes
+        torch.manual_seed(1)
+
+        if local_rank > 0:
+            sys.stdout = open(os.devnull, "w")
+
+        start_time = time.time()
+        checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
+        assert world_size == len(
+            checkpoints
+        ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is {world_size}"
+        ckpt_path = checkpoints[local_rank]
+        print("Loading")
+        checkpoint = torch.load(ckpt_path, map_location="cpu")
+        with open(Path(ckpt_dir) / "params.json", "r") as f:
+            params = json.loads(f.read())
+
+        model_args: ModelArgs = ModelArgs(
+            max_seq_len=max_seq_len, max_batch_size=max_batch_size, **params
+        )
+        tokenizer = Tokenizer(model_path=tokenizer_path)
+        model_args.vocab_size = tokenizer.n_words
+        torch.set_default_tensor_type(torch.cuda.HalfTensor)
+        model = Transformer(model_args)
+        torch.set_default_tensor_type(torch.FloatTensor)
+        model.load_state_dict(checkpoint, strict=False)
+
+        generator = LLaMA(model, tokenizer)
+        print(f"Loaded in {time.time() - start_time:.2f} seconds")
+        return generator
+
     def __init__(self, model: Transformer, tokenizer: Tokenizer):
         self.model = model
         self.tokenizer = tokenizer

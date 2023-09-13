e1c3f8caef7d0dcf10565646af55900bca24a12a
Don Mahurin <@>
2023-09-13 16:05:02 -0700

    correct rot-emb debug to be unique for q and v

--- llama/model.py
+++ llama/model.py
@@ -171,13 +171,13 @@ def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
 def apply_rotary_emb(
     x: torch.Tensor,
     freqs_cis: torch.Tensor,
-    start_pos = None, layer_id = None, head = None
+    name, start_pos = None, layer_id = None, head = None
 ) -> Tuple[torch.Tensor, torch.Tensor]:
     x = x.to(freqs_cis.device)
     x_ = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2))
     freqs_cis = reshape_for_broadcast(freqs_cis, x_)
     x_out = torch.view_as_real(x_ * freqs_cis).flatten(x.ndim-1)
-    debug_two(x_out, x, 'rot-emb', start_pos, layer_id, head)
+    debug_two(x_out, x, name, start_pos, layer_id, head)
     return x_out.type_as(x).to(device)
 
 
@@ -259,8 +259,8 @@ class Attention(nn.Module):
 
         self.wo.start_pos = start_pos
 
-        xq = torch.stack([apply_rotary_emb(self.wq[h](x), freqs_cis=freqs_cis, start_pos=start_pos, layer_id=self.layer_id, head=h) for h in range(len(self.wq))], dim=2)
-        xk = torch.stack([apply_rotary_emb(self.wk[h](x), freqs_cis=freqs_cis, start_pos=start_pos, layer_id=self.layer_id, head=h) for h in range(len(self.wk))], dim=2)
+        xq = torch.stack([apply_rotary_emb(self.wq[h](x), freqs_cis=freqs_cis, name='rot-emb-q', start_pos=start_pos, layer_id=self.layer_id, head=h) for h in range(len(self.wq))], dim=2)
+        xk = torch.stack([apply_rotary_emb(self.wk[h](x), freqs_cis=freqs_cis, name='rot-emb-k', start_pos=start_pos, layer_id=self.layer_id, head=h) for h in range(len(self.wk))], dim=2)
         xv = torch.stack([wv(x) for wv in self.wv], dim=2)
 
         xq = xq.permute(2, 0, 1, 3)
